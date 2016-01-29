6bfc967d32e790635ade0b74c3e8402057f34230
Chetan Shenoy <me@chetanshenoy.com>
2016-01-29 15:57:34 -0500

    Updated for react-router 2
    createHistory is not a function anymore.

--- modules/utils/createUseScroll.js
+++ modules/utils/createUseScroll.js
@@ -1,68 +1,66 @@
 export default function createUseScroll(updateScroll, start, stop) {
   return function (createHistory) {
-    return function (options) {
-      const history = createHistory(options)
+    const history = createHistory
 
-      let numListeners = 0
+    let numListeners = 0
 
-      function checkStart() {
-        if (++numListeners === 1 && start) {
-          start(history)
-        }
+    function checkStart() {
+      if (++numListeners === 1 && start) {
+        start(history)
+      }
+    }
+
+    function checkStop() {
+      if (--numListeners === 0 && stop) {
+        stop()
+      }
+    }
+
+    function listenBefore(hook) {
+      checkStart()
+      const unlisten = history.listenBefore(hook)
+
+      return function () {
+        unlisten()
+        checkStop()
+      }
+    }
+
+    let listeners = [], currentLocation, unlisten
+
+    function onChange(location) {
+      currentLocation = location
+
+      listeners.forEach(listener => listener(location))
+      updateScroll(location)
+    }
+
+    function listen(listener) {
+      checkStart()
+
+      if (listeners.length === 0) {
+        unlisten = history.listen(onChange)
       }
 
-      function checkStop() {
-        if (--numListeners === 0 && stop) {
-          stop()
-        }
-      }
+      // Add the listener to the list afterward so we can manage calling it
+      // initially with the current location.
+      listeners.push(listener)
+      listener(currentLocation)
 
-      function listenBefore(hook) {
-        checkStart()
-        const unlisten = history.listenBefore(hook)
-
-        return function () {
-          unlisten()
-          checkStop()
-        }
-      }
-
-      let listeners = [], currentLocation, unlisten
-
-      function onChange(location) {
-        currentLocation = location
-
-        listeners.forEach(listener => listener(location))
-        updateScroll(location)
-      }
-
-      function listen(listener) {
-        checkStart()
-
+      return function () {
+        listeners = listeners.filter(item => item !== listener)
         if (listeners.length === 0) {
-          unlisten = history.listen(onChange)
+          unlisten()
         }
 
-        // Add the listener to the list afterward so we can manage calling it
-        // initially with the current location.
-        listeners.push(listener)
-        listener(currentLocation)
-
-        return function () {
-          listeners = listeners.filter(item => item !== listener)
-          if (listeners.length === 0) {
-            unlisten()
-          }
-
-          checkStop()
-        }
+        checkStop()
       }
+    }
 
-      return {
-        ...history,
-        listenBefore,
-        listen
-      }
+    return {
+      ...history,
+      listenBefore,
+      listen
     }
   }
 }
